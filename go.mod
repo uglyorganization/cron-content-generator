3c05f66647918831e130f43f42364ef6b00b7e5d
Arnau Diaz <diazarnau0@gmail.com>
2024-03-12 12:18:39 +0100

    .

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/uglyorganization/cron-content-generator
 
 go 1.17
+
+require golang.org/x/net v0.22.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,105 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"golang.org/x/net/html"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// Read HTML content from a file
+	filePath := "file.html"
+	htmlContent, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal("Error reading HTML file:", err)
+		return
+	}
+
+	doc, err := html.Parse(strings.NewReader(string(htmlContent)))
+	if err != nil {
+		fmt.Println("Error parsing HTML:", err)
+		return
+	}
+
+	// Select all divs with the specified structure
+	selectedDivs := findDivs(doc, "post")
+	for _, div := range selectedDivs {
+		postID := div.Attr[0].Val
+		author := findAttribute(div, "data-autor")
+		// content := extractContent(div)
+
+		fmt.Printf("Post ID: %sAuthor: %s\n", postID, author)
+	}
+	fmt.Println(len(selectedDivs))
+}
+
+func findDivs(n *html.Node, className string) map[string]*html.Node {
+	divs := make(map[string]*html.Node)
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "div" && hasClass(n, className) {
+			postID := findNumericPostID(n.Attr)
+			if postID != "" {
+				divs[postID] = n
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(n)
+	return divs
+}
+
+func findNumericPostID(attrs []html.Attribute) string {
+	for _, attr := range attrs {
+		if attr.Key == "id" && strings.HasPrefix(attr.Val, "post-") {
+			return strings.TrimPrefix(attr.Val, "post-")
+		}
+	}
+	return ""
+}
+
+func hasClass(n *html.Node, className string) bool {
+	for _, attr := range n.Attr {
+		if attr.Key == "class" && strings.Contains(attr.Val, className) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func findAttribute(n *html.Node, attributeName string) string {
+	for _, attr := range n.Attr {
+		if attr.Key == attributeName {
+			return attr.Val
+		}
+	}
+
+	return ""
+}
+
+func extractContent(n *html.Node) string {
+	var content string
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.TextNode {
+			content += n.Data
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(n)
+	return content
 }
